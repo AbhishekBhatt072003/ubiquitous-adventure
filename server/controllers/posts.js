c4be721e6c31f7ac817998d4875d48094ab493bf
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-03 22:47:43 +0530

    implemented delete functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -40,3 +40,12 @@ export const updatePost = async (req, res) => {
     const updatePost = await postMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
     res.json(updatePost);
 }
+
+export const deletePost = async (req, res) => {
+    const { id: _id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send("No post with that id");
+
+    await postMessage.findByIdAndRemove(_id);
+    res.json({ message: "Post deleted successfully" });
+}
