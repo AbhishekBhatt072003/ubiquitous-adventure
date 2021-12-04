eca2150725d3d4e13ec686b4d716deb3f9a657bc
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-04 09:08:40 +0530

    added like count and delete functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,7 +7,6 @@ export const getPosts = async (req, res) => {
     try {
         const postMessages = await postMessage.find();
         res.status(200).json(postMessages);
-
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
@@ -16,12 +15,8 @@ export const getPosts = async (req, res) => {
 
 // ------------------------for creating new posts--------------------
 export const createPost = async (req, res) => {
-
     const post = req.body;
-
     const newPost = new postMessage(post);
-
-
     try {
         await newPost.save();
         res.status(201).json(newPost);
@@ -36,16 +31,28 @@ export const updatePost = async (req, res) => {
     const post = req.body;
     if (!mongoose.Types.ObjectId.isValid(_id))
         return res.status(404).send("No post with that id");
-
     const updatePost = await postMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
     res.json(updatePost);
 }
 
-export const deletePost = async (req, res) => {
-    const { id: _id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(_id))
-        return res.status(404).send("No post with that id");
 
-    await postMessage.findByIdAndRemove(_id);
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send("No post with that id");
+    await postMessage.findByIdAndRemove(id);
     res.json({ message: "Post deleted successfully" });
+}
+
+
+
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send("No post with that id");
+    const post = await postMessage.findById(id);
+    const updatedPost = await postMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import postRoutes from './routes/posts.js'
-
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 
 
@@ -18,10 +19,10 @@ app.use('/posts', postRoutes);
 
 //  ---------------------------------------- Database------------------------
 
-const CONNECTION_URL = 'mongodb+srv://abhishekbhatt:ATKPR0508R@cluster0.dggsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 // mongoose.set('useFindAndModify', false);
